ff09364f6038b09a98f130375d69d93374320657
Seifer23 <salvador.rodo@gmail.com>
2023-05-18 13:57:38 +0200

    ncmpcpp: small bug on notify-script

--- .config/ncmpcpp/ncmpcpp_notify.sh
+++ .config/ncmpcpp/ncmpcpp_notify.sh
@@ -7,28 +7,28 @@ readonly MUSIC_DIR="${HOME}/Music/"
 readonly SONG_PATH="$(mpc --format '%file%' current)"
 readonly SONG_DIR="$(dirname "${SONG_PATH}")"
 readonly ALBUM_ART_PATH="${MUSIC_DIR}/${SONG_DIR}/cover.jpg"
+readonly FALLBACK_DIR="${MUSIC_DIR}/fallback.jpg"
 
-if [[ ! -f "${ALBUM_ART_PATH}" ]]; then # if cover.jpg doesn't exist
+cover_path=""
 
-    # First we check if the audio file has an embedded album art
-    ext="$(mpc --format %file% current | sed 's/^.*\.//')"
-    if [ "$ext" = "flac" ]; then
-        # since FFMPEG cannot export embedded FLAC art we use metaflac
-        metaflac --export-picture-to=/tmp/mpd_cover.jpg \
-            "$(mpc --format "$MUSIC_DIR"/%file% current)" &&
-            cover_path="/tmp/mpd_cover.jpg" && return
-    else
-        ffmpeg -loglevel quiet -y -i "$(mpc --format "$MUSIC_DIR"/%file% | head -n 1)" \
-            /tmp/mpd_cover.jpg &&
-            cover_path="/tmp/mpd_cover.jpg" && return
-    fi
-
-    # If we still failed to find a cover image, we use the fallback
-    if [ -z "$cover_path" ]; then
-        cover_path=$fallback_image
-    fi
+# First we check if the audio file has an embedded album art
+ext="$(mpc --format %file% current | sed 's/^.*\.//')"
+if [ "$ext" = "flac" ]; then
+	# since FFMPEG cannot export embedded FLAC art we use metaflac
+	metaflac --export-picture-to=/tmp/mpd_cover.jpg \
+   	"$(mpc --format "$MUSIC_DIR"/%file% current)" &&
+	cover_path="/tmp/mpd_cover.jpg"
 else
+	ffmpeg -loglevel quiet -y -i "$(mpc --format "$MUSIC_DIR"/%file% | head -n 1)" \
+	/tmp/mpd_cover.jpg &&
+	cover_path="/tmp/mpd_cover.jpg"
+fi
+
+if [ -f "${ALBUM_ART_PATH}" ] && [ "${cover_path}" == "" ]; then # if we can't find embeded album art go for cover.jpg
 	cover_path=${ALBUM_ART_PATH}
+elif [ ! -f "${ALBUM_ART_PATH}" ] && [ "${cover_path}" == "" ]; then
+	# if everything fails use fallback image
+	cover_path=$FALLBACK_DIR
 fi
 
 notify-send -i "${cover_path}" "Now Playing" "$(mpc --format '%title% - %artist%' current)"
